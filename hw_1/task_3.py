06dd47eeece61d497d79398c14f103033143aa35
SergeiPre <63100503+SergeiPre@users.noreply.github.com>
2020-05-17 16:26:04 +0300

    Delete task_3.py

--- hw_1/task_3.py
+++
@@ -1,9 +0,0 @@
-"""
-3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
- Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
-"""
-user_number = input('Введите число: ')
-user_number = int(user_number)
-
-result = (user_number + user_number * 11 + user_number * 111)
-print(f'Сумма чисел равна: {result}')
