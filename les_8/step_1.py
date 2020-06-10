a090af3602e93045c31a10145376b3e0c6739250
Sergei Pre <presrg@gmail.com>
2020-06-10 15:12:05 +0300

    created three empty files. i'll do this HW in the near future!

---
+++ les_8/step_1.py
@@ -0,0 +1,29 @@
+class MyCls:
+    a = 22
+    b = 33
+
+    def __init__(self, data: str, a, b):
+        self.data = data
+        self.a = a
+        self.b = b
+
+
+    @staticmethod
+    def some(a, b):
+        return a + b
+
+    @classmethod
+    def some_cls(cls):
+        return cls.a + cls.b
+
+if __name__ == '__main__':
+    a = MyCls('some name', 2, 5)
+    print(MyCls.__dict__)
+    print(MyCls.__module__)
+    print(MyCls.__name__)
+
+    print(a.a + a.b)
+    print(MyCls.some_cls())
+
+    # print(a.some(2, 3))
+    # print(MyCls.some(2, 6))
