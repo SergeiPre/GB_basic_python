e7301d7c935c171f94ea0346d4248f05d5ae069e
Sergei Pre <presrg@gmail.com>
2020-06-14 13:40:10 +0300

    task_4 of hw_4 has been done

--- les_4/step_3.py
+++ les_4/step_3.py
@@ -1,8 +1,8 @@
-from my_module import sum
-print('END IMPORT')
+# from my_module import sum
+# print('END IMPORT')
 
-# print(my_module.r_sum([1, 2, 3, 4, 5]))
-print(sum(1, 2, 3, 4, 5))
+# # print(my_module.r_sum([1, 2, 3, 4, 5]))
+# print(sum(1, 2, 3, 4, 5))
 
 if __name__ == '__main__':
     print(__name__)
